e3296bdd70de4829e05e44eaad8f0d1b63fa57e9
Zidan <zidanraka99@gmail.com>
2025-11-21 16:40:54 +0700

    Create view login and animation

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,15 @@
 /// <reference types="vite/client" />
 
 type RendererDatabaseResponse<T = unknown> =
-  | { success: true; rows?: T[]; serverTime?: unknown }
+  | { success: true; rows?: T[]; serverTime?: unknown; user?: T }
   | { success: false; message: string }
 
 interface Window {
   database: {
     testConnection: () => Promise<RendererDatabaseResponse>
     fetchTableData: <T = unknown>(tableName: string) => Promise<RendererDatabaseResponse<T>>
+    login: (
+      payload: { username: string; password: string; division?: string | null },
+    ) => Promise<RendererDatabaseResponse<{ username: string; division?: string | null; name?: string | null }>>
   }
 }
